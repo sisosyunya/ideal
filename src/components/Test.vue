a0fe0ebceb35ba4399afa699fbc14f55a2f0e3d3
sisosyunya <85158805+sisosyunya@users.noreply.github.com>
2021-09-20 17:57:35 +0900

    asaa

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>aaaa</h2>
+</template>
+
+
+<script>
+export default {
+  name: 'Test',
+   
+
+}
+</script>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+        <!-- <Test msg="Welcome to Your Vue.js App"/> -->
+        <Test/>
   </div>
 </template>
+<script>
+import Test from "@/components/Test.vue";
+//使えるようにする
+export default{
+  components:{Test}
+};
+console.log("aaa")
+</script>
+
