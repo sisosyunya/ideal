bdf313f71398bbcba9c5526b8889f1dbaa60bc0a
sisosyunya <0710syunya@gmail.com>
2021-10-16 01:21:14 +0900

    Googleアカウントでもログインできるよー

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,6 +8,9 @@
     <router-link to="/home">アイデアをみる</router-link> | 
         <router-link to="/login">ログイン</router-link> |
   </div>
+  <div class="google">
+  <button @click="googleSignin">googleアカウントでログイン</button>
+  </div>
   <div class="sign">
       <table>
           <tr>
@@ -33,20 +36,33 @@
 <script>
 // import { firebase } from "firebase/firestore"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+
+import {signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+
+
 export default{
   methods:{
-   async doLogin(){
+    async doLogin(){
       // firebase.auth().signIn
         const auth = getAuth();
         // const user = userCredential.user;
         await signInWithEmailAndPassword(auth,this.email,this.password)
         .then(
+          //成功時の処理
           alert('success')
         )
         .catch(
+          //失敗の処理
           alert('lose')
         )
-      }
+      },
+   async googleSignin(){
+     const provider = new GoogleAuthProvider();
+     const auth = getAuth();
+     await signInWithPopup(auth, provider)
+
+   }
     }
   }
 
