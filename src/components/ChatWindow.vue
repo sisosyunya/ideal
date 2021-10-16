bf56e7505a495d2d447272b27d92aed55cfe4623
sisosyunya <0710syunya@gmail.com>
2021-10-16 12:15:42 +0900

    一応チャットやってみた

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="chat-window">
+    <div v-if="error">{{ error }}</div>
+    <div v-if="documents" class="message" ref="messages">
+      <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+        <span class="created-at">{{ doc.createdAt }}</span>
+        <span class="name">{{ doc.name }}</span>
+        <span class="message">{{ doc.message }}</span>
+      </div>
+    </div>
+  </div>
+</template>
