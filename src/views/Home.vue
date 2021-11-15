333ea7c3f1ada2d3182bf65bfc2c56ba25db8327
hiorkiwa <hirokifsys@gmail.com>
2021-11-16 03:16:07 +0900

    hirokiwakuwaku

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,22 +7,43 @@
     <router-link to="/post">アイデアをだす</router-link> |
     <router-link to="/home">アイデアをみる</router-link> |
     <router-link to="/login">ログイン</router-link> |
-        <router-link to="/mypage">マイページ</router-link> |
   </div>
   <main>
-    <div class="title">
-      カテゴリ
-    </div>
+    <div class="title">カテゴリ</div>
     <div>
-      <select v-model="category" class="category">
+      <!-- <option>選択してください▼</option> -->
+      <!-- <select v-model="category" id="selectCategory" class="category">
         <option disabled value="">選択してください▼</option>
+        <option>全て</option>
         <option>自由研究</option>
         <option>読書感想文</option>
+        selectCategories[selectedCategory-1].label
+      </select> -->
+      <select v-model="category" @change="fetchIdeasct(selectCategories[category].label)">
+        <option disabled value="">選択してください▼</option>
+        <option
+          v-for="category in selectCategories"
+          :value="category.id"
+          :key="category.id"
+        >
+          {{ category.label }}
+        </option>
       </select>
-    </div>
-  </main>
 
-  <div class="container">
+    </div>
+    <!-- 関数実行テストボタン -->
+    <!-- <button v-on:click="fetchIdeasct('自由研究')">実行</button> -->
+  </main>
+  <!-- 自由研究表示 -->
+  <div v-if="search" class="container">
+    <v-card v-for="(ideas, i) in ideas" :key="i" :idea="idea" />
+  </div>
+  <!-- 読書感想文表示 -->
+  <div v-else-if="read" class="container">
+    <v-card v-for="(idea, i) in ideas" :key="i" :idea="idea" />
+  </div>
+  <!-- すべて表示 -->
+  <div v-else class="container">
     <v-card v-for="(idea, i) in ideas" :key="i" :idea="idea" />
   </div>
 </template>
@@ -31,25 +52,59 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../main";
 import VCard from "../components/VCard.vue";
+import { query, where } from "firebase/firestore";
 
 export default {
   name: "Home",
+  // search: false,
+  // read: false,
   components: {
     VCard,
   },
-  data: () => ({
-    ideas: [],
-    category: "",
-  }),
+  // select = document.getElementById('selectCategory'),
+  // select.onchange = function(){
+  // alert(this.value);
+  // },
+  data :()=> {
+    return{
+      search: false,
+      read: false,
+      ideas: [],
+      selectCategories : [
+        { id: 0, label: "自由研究" },
+        { id: 1, label: "読書感想文" },
+      ],
+      category : 0
+
+    }
+  },
   mounted() {
     this.fetchIdeas();
-    ///mountedってやつでvcardが元から表示されるみたい
   },
   methods: {
     async fetchIdeas() {
       const querySnapshot = await getDocs(collection(db, "ideas"));
       querySnapshot.forEach((doc) => {
         this.ideas.push({ ...doc.data(), id: doc.id });
+        console.log({ ...doc.data(), id: doc.id });
+      });
+    },
+    // ひろき作業中↓↓↓↓↓↓↓↓
+    async fetchIdeasct(category) {
+      // const ideasct = collection(db, "ideas");
+      // remove() {
+      console.log(this.ideas.length);
+      this.ideas.splice(0, this.ideas.length);
+      const queryct = query(
+        collection(db, "ideas"),
+        where("category", "==", category),
+      );
+      console.log(this.category)
+      const queryctSnapshot = await getDocs(queryct);
+      queryctSnapshot.forEach((doc) => {
+        this.ideas.push({ ...doc.data(), id: doc.id });
+        console.log({ ...doc.data(), id: doc.id }),
+          console.log(this.search === true);
       });
     },
   },
@@ -63,7 +118,6 @@ export default {
   gap: 10px;
   max-width: 1024px;
   margin: 0 auto;
-  margin-bottom: 100px;
 }
 .category {
   padding: 5px 10px;
@@ -98,3 +152,4 @@ main {
   transition: 0.3s;
 }
 </style>
+
