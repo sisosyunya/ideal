5a9a92b9528a5baf8d0635cfe824c4c80c34867e
hirokiwa <hirokifsys@gmail.com>
2022-11-24 20:41:58 +0900

    edit server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+var app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server started '+ port);
